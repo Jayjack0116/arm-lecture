713d09d63b25ad363e665a693c72ba67bd60d4ae
Jay <opudding7654321@gmail.com>
2015-03-13 23:35:57 +0800

    Add time unit in the string.
    Add the time unit "second" after the variable "duration ".

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -14,7 +14,9 @@ int main(int argc, char **argv)
   start_time =clock();
   result = fibonacci(number);   
   end_time=clock();
+
   duration = (double) (end_time - start_time) / CLOCKS_PER_SEC; //the unit of clock() returned value is millisecond
+  
   printf("The fibonacci sequence at %d is: %d\n", number, result);
   printf("The duration running fibonacci function is %lf second\n", duration );
 }
