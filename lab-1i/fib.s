57e3f6e1d69a6aa156c1583b65cdc541ac3e6ab8
Jay <opudding7654321@gmail.com>
2015-03-13 23:33:19 +0800

    Accomplsh the fib.s and test the efficiency of non-recursive fibonacci.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -14,19 +14,36 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	subs r4, r0, #0;
+
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	ble .L3;
 
 	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r4, #1;
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	@ If R4 == 1 goto .L4 (which returns 1)
+	beq .L4;
+	mov r4, r0;
+	mov r3, #0;
+	mov r5, #1;
+
+for:
+	@ R0 = R3 + R5
+	adds r0, r3, r5;
+
+	@ R3 = R5
+	mov r3, r5;
 
 	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	mov r5, r0;
 
-	@ R0 = R5 + R0 (update flags)
+	@ R4 = R4 - 1
+	subs r4, r4, #1;
+
+	@ If R4 > 1 goto for
+	cmp r4, #1;
+	bgt for;
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
