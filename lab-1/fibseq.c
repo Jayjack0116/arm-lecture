4b7413a48f449d1a836712b7693b48d7feb29c68
Jay <opudding7654321@gmail.com>
2015-03-13 22:16:58 +0800

    Accomplish fib.s and add clock() fuction to test the duration of operation of fibonacci.
    reference:https://hackpad.com/Lab38-Report-dfYHWqxmqJ8
    
    I modified the passing argument in Makefile.We can use function clock() to compute
    the processor time consumed by the program.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,9 +7,15 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
-
+  clock_t start_time, end_time;
+  double duration = 0;
   scanf("%d",&number);
+
+  start_time =clock();
   result = fibonacci(number);   
+  end_time=clock();
+  duration = (double) (end_time - start_time) / CLOCKS_PER_SEC; //the unit of clock() returned value is millisecond
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  printf("The duration running fibonacci function is %lf second\n", duration );
 }
 
